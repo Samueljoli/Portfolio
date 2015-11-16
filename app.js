7d09119124b8ec853fa0ff57794868d75570a3e6
Samueljoli <samuel.joli.ftn@gmail.com>
2015-11-16 17:56:33 -0500

    Add angular to news ticker

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    sectionsColor: ['deeppink', '#4BBFC3', '#7BAABE'],
+    sectionsColor: ['#F8F8F8', '#4BBFC3', '#7BAABE'],
     anchors: ['Home', 'Work', 'Technologies', 'Contact'],
     
     });
 });
+
+
+
+var ScrollApp = angular.module('ScrollApp', []);
+
+ScrollApp.controller('PreviewController', function($scope) {
+  $scope.$watch('input', function(val) {
+    $scope.output = val;
+}, true);
+
+  $scope.input = "Insert your most insanely clever, thought provoking quote here."
+  $scope.output = ""
+});
+
+ScrollApp.filter('html', function($sce) {return $sce.trustAsHtml; });
+
+
+
+
