07065064e8b77a057f1d14e41df2ad4b6503cd41
Samueljoli <samuel.joli.ftn@gmail.com>
2015-11-16 01:25:20 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsColor: ['deeppink', '#4BBFC3', '#7BAABE'],
+    anchors: ['Home', 'Work', 'Technologies', 'Contact'],
+    
+    });
+});
