8dacf6e747b907068a132b0c2c9dd6e7fb8e3868
Samueljoli <samuel.joli.ftn@gmail.com>
2015-11-18 13:17:46 -0500

    Create work slide one

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-$(document).ready(function() {
+var ScrollApp = angular.module('ScrollApp', []);
+
+ScrollApp.controller('PreviewController', function($scope) {
+  $scope.$watch('input', function(val) {
+    $scope.output = val;
+  }, true);
+
+  $scope.input = "Insert your most insanely clever, thought provoking quote here."
+  $scope.output = ""
+});
+
+ScrollApp.filter('html', function($sce) {return $sce.trustAsHtml; });
+
+
+
+$(document).ready(function() { 
   $('#fullpage').fullpage({
     sectionsColor: ['#F8F8F8', '#4BBFC3', '#7BAABE'],
     anchors: ['Home', 'Work', 'Technologies', 'Contact'],
     afterLoad: function(anchorLink, index) {
       if(index == 2) {
-        $('#work').fadeIn('slow').animate({
+        $('#work').fadeIn('slow').animate({ //CHECK TO SEE IF I CAN APPEND TWO JQUERYS WITHIN THE SAME PARENTHESIS
           left: '0'
         }, {duration: 'slow', queue:false}, function() {
 
@@ -19,22 +34,6 @@ $(document).ready(function() {
   });
 });
 
-
-
-
-
-var ScrollApp = angular.module('ScrollApp', []);
-
-ScrollApp.controller('PreviewController', function($scope) {
-  $scope.$watch('input', function(val) {
-    $scope.output = val;
-}, true);
-
-  $scope.input = "Insert your most insanely clever, thought provoking quote here."
-  $scope.output = ""
-});
-
-ScrollApp.filter('html', function($sce) {return $sce.trustAsHtml; });
 
 
 
